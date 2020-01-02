32440b1752a9ac90f8cd59aa9d347911e07d7a13
Sara Khosravinasr <saranasr83@gmail.com>
2020-01-02 14:52:39 -0800

    change the homepage fontSize

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -33,7 +33,7 @@ class Home extends React.Component {
                             <div className="image-location">
                                 <h3 className="text-center location" style={{ marginTop: "50px", fontFamily: "sans-serif" }}>San Francisco</h3>
                                 <img className="img-fluid img-home" src={sf} alt="wework work space" />
-                                <div className="text-center class-time-loc">
+                                <div className="text-center class-time-loc" style={{ fontSize: "20px" }}>
                                     <p style={{ marginTop: "25px", color: "#1766CE" }}><b>2 locations</b></p>
                                     <p><b>Tuesdays @ 6 PM, </b><u>MindSpace 575 Market St.</u></p>
                                     {/* <p><u>MindSpace 575 Market St.</u></p> */}
@@ -53,7 +53,7 @@ class Home extends React.Component {
                             <div className="image-location">
                                 <h3 className="text-center location" style={{ marginBottom: "10px", marginTop: "50px", fontFamily: "sans-serif" }}> UC Berkeley </h3>
                                 <img className="img-fluid img-home" src={UCB} alt="UC Berkeley campaus" />
-                                <div className="text-center class-time-loc">
+                                <div className="text-center class-time-loc" style={{ fontSize: "20px" }}>
                                     <p style={{ marginTop: "25px", color: "#1766CE" }}><b>TBD</b></p>
                                     <p>New on-campus location and time </p>
                                     <p>TBD early February 2020</p>
